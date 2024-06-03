1240a6f77d828907bfa078e472be838e27da7829
coffeedev <168582458+coffeedev222@users.noreply.github.com>
2024-06-03 16:03:00 -0500

    Added Psuedocode before coding

---
+++ javascript.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+
+//Get Computer generated choice
+//Get Human input for their choice
+//Store the score into a variable to keep count of score
+//Create how round is played by taking Computers choice, and humans choice to determine the winner based on rules of rock paper scissors
+//Create how the game is won by keeping tally of the score until 5 wins is reached by one player.
